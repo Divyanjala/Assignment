b7fc2e964cb9a2552d561143445de8ec70b93cd7
Divyanjala <Diwyanjala96@gmail.com>
2022-07-23 20:51:05 +0530

    changed auth

--- resources/views/includes/police/nav.blade.php
+++ resources/views/includes/police/nav.blade.php
@@ -15,7 +15,7 @@
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{Auth::user()->name}}</span>
                 <img class="img-profile rounded-circle" src="{{ asset('theme/img/undraw_profile.svg') }}">
             </a>
             <!-- Dropdown - User Information -->
@@ -24,7 +24,7 @@
                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                     Profile
                 </a>
-               
+
                 <div class="dropdown-divider"></div>
 
 
